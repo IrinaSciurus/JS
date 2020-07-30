d1c3484144a6d09dc22e47d99ef18aca35370c73
Петруничкина Ирина Александровна <iapetrun@mts.ru>
2020-07-31 01:13:52 +0300

    Add Music player

--- iTunes JS/scripts/videoPlayer.js
+++ iTunes JS/scripts/videoPlayer.js
@@ -1,3 +1,5 @@
+import {addZero} from './suppScript.js';
+
 export const videoPlayerInit = () => {
 
 const videoPlayer = document.querySelector('.video-player');
@@ -32,9 +34,6 @@ const stopPlay = () => {
     videoPlayer.currentTime = 0;
 };
 
-const addZero = n => n < 10 ? '0' + n : n;
-
-
     videoPlayer.addEventListener('click', togglePlay) ;
     videoButtonPlay.addEventListener('click', togglePlay);
     videoButtonStop.addEventListener('click', stopPlay);
@@ -47,13 +46,14 @@ const addZero = n => n < 10 ? '0' + n : n;
     videoPlayer.addEventListener('timeupdate', () => {
         const currentTime = videoPlayer.currentTime;
         const duration = videoPlayer.duration;
+        
         videoProgress.value = (currentTime / duration) * 100;
 
-        let minutePassed = Math.floor(currentTime / 60);
-        let secondsPassed = Math.floor(currentTime % 60);
+        let minutePassed = Math.floor(currentTime / 60)  || '0';
+        let secondsPassed = Math.floor(currentTime % 60)  || '0';
 
-        let minuteTotal = Math.floor(duration / 60);
-        let secondsTotal = Math.floor(duration % 60);
+        let minuteTotal = Math.floor(duration / 60)  || '0';
+        let secondsTotal = Math.floor(duration % 60)  || '0';
 
         videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
         videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`; 
